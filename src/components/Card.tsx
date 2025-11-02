cab585cf14ff918d2820f29d580781d00da7bdee
JG-Souza <joaogsouzafma@gmail.com>
2025-11-02 00:03:47 -0300

    Arruma componente Card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,17 +2,15 @@ import React from "react";
 
 //define o formato do componente card
 export type CardProps = {
-  titulo: string;
-  descricao: string;
-  imagem: string;
+  children: React.ReactNode;
+  className?: string;
 }
+
 //:CardProps é o tipo esperado pra cada prop dessa (titulo, imagem e descricao)
-export default function Card ({titulo, descricao, imagem}: CardProps) { 
+export default function Card({ children, className = '' }: CardProps) {
   return (
-    <div className="bg-white rounded-lg shadow-md p-4 max-w-sm mx-auto">
-      <img src={imagem} alt={titulo} className="w-full h-40 object-cover rounded-md mb-4" />
-      <h2 className="text-lg font-bold text-gray-800 mb-2">{titulo}</h2>
-      <p className="text-gray-600 text-sm">{descricao}</p>
+    <div className={`bg-white rounded-lg shadow-md p-4 ${className}`}>
+      {children}
     </div>
-  )
+  );
 }
