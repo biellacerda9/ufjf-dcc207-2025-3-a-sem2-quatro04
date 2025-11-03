5b9ee5da298cbad1d0247b1f33e29051941e0dc2
Gabriel Lacerda <gabriellacerda.carvalho@estudante.ufjf.br>
2025-11-03 14:50:42 -0300

    Implementa botão no patient header

--- src/components/PatientHeader.tsx
+++ src/components/PatientHeader.tsx
@@ -1,34 +1,32 @@
-import React from 'react';
+import React from "react";
 
-import { Avatar } from './Avatar';
-import { Heading, BodyText } from './Typography';
+import { Avatar } from "./Avatar";
+import { Heading, BodyText } from "./Typography";
+import Button from "./button";
 
 type Paciente = {
   nome: string;
   fotoUrl: string;
-}
+};
 
 type PatientHeaderProps = {
   paciente: Paciente;
-}
+};
 
 export default function PatientHeader({ paciente }: PatientHeaderProps) {
   return (
-    <div className="flex items-center p-4 mb-6 bg-blue-50 rounded-lg shadow-sm">
-      
+    <div className="flex items-center justify-between p-4 mb-6 bg-blue-50 rounded-lg shadow-sm">
+      <div className="flex items-center gap-3">
       <Avatar src={paciente.fotoUrl} alt={paciente.nome} />
-
-      <div className="ml-4">
-        
-        <Heading className="!text-2xl">
-          {`Olá, ${paciente.nome}!`}
-        </Heading>
-        
-        <BodyText className="!text-base text-gray-600">
-          Plano de Medicação de Hoje
-        </BodyText>
+        <div>
+          <Heading className="!text-2xl">{`Olá, ${paciente.nome}!`}</Heading>
+          <BodyText className="!text-base text-gray-600">
+            Plano de Medicação de Hoje
+          </BodyText>
+        </div>
       </div>
 
+      <Button texto="Adicionar Medicamento" />
     </div>
   );
 }
