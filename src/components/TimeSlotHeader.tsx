929abda3194a1ff6581f80b485902e1059ccb2f8
JG-Souza <joaogsouzafma@gmail.com>
2025-11-03 13:49:27 -0300

    Pequena correção de digitação

--- src/components/TimeSlotHeader.tsx
+++ src/components/TimeSlotHeader.tsx
@@ -10,7 +10,7 @@ export default function TimeSlotHeader ({titulo, icone}: TimeSlotHeaderProps) {
     return (
         <div className="flex items-center justify-center gap-2 p-2 bg-blue-50 rounded-lg shadow-sm">
             {/* mostra o emoji e aumenta o tamanho dele */}
-            <Icon name={icone} className="text-2x1"/> 
+            <Icon name={icone} className="text-2xl"/> 
             {/* mostra o subtitulo */}
             <Subheading>{titulo}</Subheading>
         </div>
