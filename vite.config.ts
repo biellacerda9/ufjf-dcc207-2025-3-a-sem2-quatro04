057a2a9818585d561ba325fec6f7c9553623f0db
JG-Souza <joaogsouzafma@gmail.com>
2025-11-06 11:40:33 -0300

    Teste deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/ufjf-dcc207-2025-3-a-sem01-404",
 })
